302d0579fbb674682c5cf9c6402efdc476e7cc4a
Shun Hao <teoshunhao@gmail.com>
2024-06-16 10:52:50 +0800

    Web App partially functioning
    added in a super basic Index and product list.

---
+++ Sellebrate/myapp/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import *
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['ProductID', 'ProductName', 'Category', 'UnitPrice']
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['CustomerID', 'Name', 'Email', 'ContactNumber', 'Address', 'Country', 'LastPurchaseDate']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['OrderID', 'CustomerID', 'OrderDate', 'TotalAmount', 'ShippingAddress']
+
+class InventoryForm(forms.ModelForm):
+    class Meta:
+        model = Inventory
+        fields = ['ProductID', 'StockQuantity', 'LastRestocked']
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['OrderID', 'PaymentMethodID', 'PaymentStatus', 'PaymentDate', 'Amount']
+
+class OrderdetailForm(forms.ModelForm):
+    class Meta:
+        model = OrderDetail
+        fields = ['OrderID', 'ProductID', 'Quantity', 'UnitPrice']
+
+class PaymentmethodForm(forms.ModelForm):
+    class Meta:
+        model = PaymentMethod
+        fields = ['CustomerID', 'MethodType', 'Provider', 'ExpiryDate']
--- Sellebrate/myapp/views.py
+++ Sellebrate/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
-# Create your views here.
+def index(request):
+    return render(request, 'retail/index.html')
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'retail/product_list.html', {'products' : products})
