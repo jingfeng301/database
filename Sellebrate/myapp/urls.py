71983ce5dd52acecb696721ca34e087012f000e5
Shun Hao <teoshunhao@gmail.com>
2024-06-16 13:11:54 +0800

    Order Status and Product List cleanup
    Can view past order status, and each order detail.
    Also added ability to just view 50 item each with next page and previous page functionality

--- Sellebrate/myapp/urls.py
+++ Sellebrate/myapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('products/', views.product_list, name = 'product_list'),
+    path('orders/', views.order_list, name = 'order_list'),
+    path('orders/<str:order_id>/', views.order_status, name = 'order_detail'),
 ]
--- Sellebrate/myapp/views.py
+++ Sellebrate/myapp/views.py
@@ -1,10 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import *
 from .forms import *
 
 def index(request):
     return render(request, 'retail/index.html')
 
+#Product Management - Create/Sell/Remove products
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'retail/product_list.html', {'products' : products})
+    product_list = Product.objects.all()
+    paginator = Paginator(product_list, 50)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'retail/product_list.html', {'page_obj': page_obj})
+
+#def product_detail(request):
+
+#def product_delete(request):
+
+#def product_create(request):
+
+#Order Management - View current order status
+def order_list(request):
+    order_list = Order.objects.all()
+    paginator = Paginator(order_list, 50) 
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'retail/order_list.html', {'page_obj': page_obj})
+
+
+def order_status(request,order_id):
+    order = get_object_or_404(Order, OrderID=order_id)
+    order_details = OrderDetail.objects.filter(OrderID = order)
+    return render(request, 'retail/order_detail.html', {'order': order, 'order_details' : order_details})
+
+#Customer - Create new user
+#Login Page
+
+#View past order as customer
+#View past purchase/transaction as admin
+
+#Update current stock and price and description of product
+#Search Filter
