a0876a8a0c56212e9924913fe290697e9d7dd3d4
zvexl <pierrechan32@gmail.com>
2024-08-03 18:15:07 +0800

    Complete queries for average rating for each product, top rated products, most reviewed products and top rated for the past year.

--- Sellebrate/myapp/views.py
+++ Sellebrate/myapp/views.py
@@ -17,7 +17,13 @@ from .mongo_utils import *
 import logging
 from bson import ObjectId
 from django.http import Http404
+from django.shortcuts import render
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 logger = logging.getLogger(__name__)
 
 #SUPPORT SYSTEM
@@ -818,6 +824,75 @@ def index(request):
             for row in stock_turnover_rate
         ]
 
+        # For NoSQL insights on Reviews
+        mongo_uri = os.getenv("MONGO_DB_URI")
+        db_name = os.getenv("MONGO_DB_NAME")
+        
+        client = MongoClient(mongo_uri)
+        db = client[db_name]
+
+        average_rating_pipeline = [
+            {"$group": {"_id": "$ProductID", "average_rating": {"$avg": "$Rating"}}}
+        ]
+
+        top_rated_products_pipeline = [
+            {"$group": {"_id": "$ProductID", "average_rating": {"$avg": "$Rating"}}},
+            {"$sort": {"average_rating": -1}},
+            {"$limit": 5}
+        ]
+
+        most_reviewed_products_pipeline = [
+            {"$group": {"_id": "$ProductID", "review_count": {"$sum": 1}}},
+            {"$sort": {"review_count": -1}},
+            {"$limit": 5}
+        ]
+        
+        # current_year = datetime.now().year
+        # top_rated_of_the_year_pipeline = [
+        #     {
+        #         "$match": {
+        #             "ReviewDate": {
+        #                 "$gte": datetime(current_year, 1, 1),
+        #                 "$lt": datetime(current_year + 1, 1, 1)
+        #             }
+        #         }
+        #     },
+        #     {"$group": {"_id": "$ProductID", "average_rating": {"$avg": "$Rating"}}},
+        #     {"$sort": {"average_rating": -1}},
+        #     {"$limit": 5}
+        # ]
+        
+        one_year_ago = datetime.now() - timedelta(days=365)
+        top_rated_for_the_past_year_pipeline = [
+        {"$match": {"ReviewDate": {"$gte": one_year_ago}}},
+        {"$group": {"_id": "$ProductID", "average_rating": {"$avg": "$Rating"}}},
+        {"$sort": {"average_rating": -1}},
+        {"$limit": 5}
+        ]
+
+        average_ratings = list(db.review.aggregate(average_rating_pipeline))
+        top_rated_products = list(db.review.aggregate(top_rated_products_pipeline))
+        most_reviewed_products = list(db.review.aggregate(most_reviewed_products_pipeline))
+        top_rated_for_the_past_year = list(db.review.aggregate(top_rated_for_the_past_year_pipeline))
+
+        # Rename _id field to product_id
+        for item in average_ratings:
+            item['product_id'] = item.pop('_id')
+        for item in top_rated_products:
+            item['product_id'] = item.pop('_id')
+        for item in most_reviewed_products:
+            item['product_id'] = item.pop('_id')
+        for item in top_rated_for_the_past_year:
+            item['product_id'] = item.pop('_id')
+
+        insights.update({
+            "average_ratings": average_ratings,
+            "top_rated_products": top_rated_products,
+            "most_reviewed_products": most_reviewed_products,
+            "top_rated_for_the_past_year": top_rated_for_the_past_year
+        })
+
+    # print(insights)  # Print insights to verify data, for logging purposes
 
     return render(request, 'retail/index.html', {'insights': insights})
 
